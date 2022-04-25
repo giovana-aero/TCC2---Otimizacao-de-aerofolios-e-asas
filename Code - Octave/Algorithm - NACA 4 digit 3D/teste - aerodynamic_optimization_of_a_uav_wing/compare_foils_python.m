d2f414283471f0bea8d1ab77c54cf0026868d13b
gustavo-011 <54364474+gustavo-011@users.noreply.github.com>
2022-04-25 17:28:22 -0300

    Delete Code - Octave/Algorithm - NACA 4 digit 3D/teste - aerodynamic_optimization_of_a_uav_wing directory

--- Code - Octave/Algorithm - NACA 4 digit 3D/teste - aerodynamic_optimization_of_a_uav_wing/compare_foils_python.m
+++
@@ -1,8 +0,0 @@
-clc,clear
-
-foil1 = dlmread('foil_python.dat');
-foil2 = dlmread('foil_octave.dat');
-isequal(foil1,foil2)
-
-%figure(1),clf
-%plot(foil1(:,1),foil1(:,3),foil2(:,1),foil2(:,3)),grid on,axis equal
