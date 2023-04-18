84d94b59c9d7c384e0119305ea877aebf7d904d7
gi-04 <54364474+gi-04@users.noreply.github.com>
2023-04-18 11:26:20 -0300

    Add files via upload

--- Code - Octave/Algorithm - NACA 4 digit 3D/algorithm_naca4_3D_AERO.m
+++ Code - Octave/Algorithm - NACA 4 digit 3D/algorithm_naca4_3D_AERO.m
@@ -20,7 +20,7 @@
 
 % A fazer:
 % Pôr na checagem de erros tudo o que for referente às opções 'L'
-% Pôr na checagem de erro tudo referente às funções objetivas 'q' e '|'
+% Pôr na checagem de erro tudo referente às funções objetivos 'q' e '|'
 % Retirar o número de reynolds deste algoritmo?
 
 
@@ -96,7 +96,7 @@ dat.coeff_F = [1,1,1,1;
                1,1,1,1;
                1,1,1,1];
 % [CL CD L/D CM] Definição de cada linha da matriz dat.coeff_op
-% '!' -> não usar como função objetiva
+% '!' -> não usar como função objetivo
 % '^' -> procurar por um valor máximo (CL e L/D) ou valor mínimo (CD)
 % 'c' -> buscar valor constante de coeficiente de momento (arbitrário)
 % 'k' -> buscar valor constante de coeficiente de momento (específico, de dat.coeff_val(1,4))
@@ -105,7 +105,7 @@ dat.coeff_F = [1,1,1,1;
 % 'q' -> procurar por um valor específico de força de sustentação, força de arrasto
 % ou momento de arfagem (CL, CD e CM). Nesse caso, definir o valor em sua casa na matriz dat.coeff_val
 % '#' -> procurar por um valor máximo (L) ou mínimo (D)
-% A matriz dat.coeff_F dá os pesos de cada função objetiva
+% A matriz dat.coeff_F dá os pesos de cada função objetivo
 
 % Checagem de erros
 dat = error_check_naca4_3D(dat);
--- Code - Octave/Algorithm - NACA 4 digit 3D/error_check_naca4_3D.m
+++ Code - Octave/Algorithm - NACA 4 digit 3D/error_check_naca4_3D.m
@@ -126,37 +126,37 @@ end
 	% end
 % end
 
-% Parâmetros das simulações: configuração das funções objetivas
+% Parâmetros das simulações: configuração das funções objetivo
 if size(dat.coeff_op,1) < dat.cases || size(dat.coeff_val,1) < dat.cases || size(dat.coeff_F,1) < dat.cases
-    error('Configurações das funções objetivas são insuficientes para todas as condições de voo')
+    error('Configurações das funções objetivo são insuficientes para todas as condições de voo')
 end
 
-% Parâmetros das simulações: função objetiva 'q' para força de sustentação, força de arrasto e momento de arfagem
+% Parâmetros das simulações: função objetivo 'q' para força de sustentação, força de arrasto e momento de arfagem
 if ismember('q',dat.coeff_op(:,3))
-	error('Função objetiva q vale apenas para a sustentação, arrasto e momento')
+	error('função objetivo q vale apenas para a sustentação, arrasto e momento')
 end
 
-% parâmetros das simulações: função objetiva '|' para força de sustentação e de arrasto
+% parâmetros das simulações: função objetivo '|' para força de sustentação e de arrasto
 if ismember('#',dat.coeff_op(:,3:4))
-	error('Função objetiva # vale apenas para sustentação e arrasto')
+	error('função objetivo # vale apenas para sustentação e arrasto')
 end
 
-% Parâmetros das simulações: funções objetivas de coeficiente de momento
+% Parâmetros das simulações: funções objetivo de coeficiente de momento
 if ismember('c',dat.coeff_op(:,1:3)) || ismember('k',dat.coeff_op(:,1:3))
-    error('Funções objetivas c e k valem apenas para o coeficiente de momento')
+    error('funções objetivo c e k valem apenas para o coeficiente de momento')
 end
 
-% Parâmetros das simulações: funções objetivas de coeficientes de momento referentes a múltiplas condições de voo
+% Parâmetros das simulações: funções objetivo de coeficientes de momento referentes a múltiplas condições de voo
 if dat.cases == 1 && dat.coeff_op(1,4) == 'c' || dat.cases == 1 && dat.coeff_op(1,4) == 'k'
-    warning('Função objetiva c/k vale apenas para múltiplas condições de voo. A mesma será ignorada.')
+    warning('função objetivo c/k vale apenas para múltiplas condições de voo. A mesma será ignorada.')
     dat.coeff_op(1,4) = '!'; pause(5)
 end
 
 if sum(sum(dat.coeff_op(1:dat.cases,:) == '!')) == numel(dat.coeff_op(1:dat.cases,:)),
-    error('Ao menos uma das funções objetivas deve estar ativa')
+    error('Ao menos uma das funções objetivo deve estar ativa')
 end
 
-% PArâmetros das simulações: funções objetivas (geral)
+% PArâmetros das simulações: funções objetivo (geral)
 T = dat.coeff_op;
 for i = 1:dat.cases
 	for j = 1:4
@@ -169,7 +169,7 @@ end
 if dat.cases > 1
 	for i = 2:dat.cases
         if sum(dat.coeff_op(i,:) == '!') == 4 && dat.coeff_op(1,4) ~= 'c' && dat.coeff_op(1,4) ~= 'k'
-			error(['Condição de voo ' num2str(i) ' não tem função objetiva definida'])
+			error(['Condição de voo ' num2str(i) ' não tem função objetivo definida'])
 		end
 	end
 end
@@ -179,13 +179,13 @@ for P = 1:dat.cases
         error(['Condição de voo ' num2str(P) ': CD alvo deve ser maior que zero'])
     elseif dat.coeff_op(P,2) == 'o' && dat.coeff_val(P,2) == 0
         dat.coeff_op(P,2) = '^';
-        warning(['Condição de voo ' num2str(P) ': CD = 0 - Função objetiva de arrasto trocada de o para ^']),pause(5)
+        warning(['Condição de voo ' num2str(P) ': CD = 0 - função objetivo de arrasto trocada de o para ^']),pause(5)
     end
 end
 
 if dat.cases > 1 && dat.coeff_op(1,4) == 'c' || dat.cases > 1 && dat.coeff_op(1,4) == 'k'
 	if sum(dat.coeff_op(2:end,4) ~= '!') ~= 0
-		warning('Função objetiva de momento c/k: funções objetivas das condições de voo subsequentes serão ignoradas'),pause(5)
+		warning('função objetivo de momento c/k: funções objetivo das condições de voo subsequentes serão ignoradas'),pause(5)
 		dat.coeff_op(2:dat.cases,4) = repmat('!',dat.cases-1,1);
 	end	
 end
