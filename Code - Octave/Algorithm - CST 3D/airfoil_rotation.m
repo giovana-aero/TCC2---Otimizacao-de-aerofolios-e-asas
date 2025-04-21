b47b952c951f551fae98a02f8e89bcd311ed5e38
giovana-aero <54364474+giovana-aero@users.noreply.github.com>
2025-04-21 17:54:59 -0300

    Update airfoil_rotation.m

--- Code - Octave/Algorithm - CST 3D/airfoil_rotation.m
+++ Code - Octave/Algorithm - CST 3D/airfoil_rotation.m
@@ -1,14 +1,14 @@
 function coo_R = airfoil_rotation(coo,th)
 
-% Centro de rotaÁ„o (sempre no ponto de quarto de corda como padr„o)
+% Centro de rota√ß√£o (sempre no ponto de quarto de corda como padr√£o)
 c = 1/4;
 
-% Fazer a rotaÁ„o
-th = -th; % As convenÁıes de sinal (matem·tica e aeron·utica) s„o inversas, portanto, realiza-se a troca aqui
+% Fazer a rota√ß√£o
+th = -th; % As conven√ß√µes de sinal (matem√°tica e aeron√°utica) s√£o inversas, portanto, realiza-se a troca aqui
 R = [cosd(th),-sind(th);
      sind(th),cosd(th)];
-coo(:,1) = coo(:,1) - c;  % Transladar as coordenadas pra que a rotaÁ„o ocorra em torno do ponto desejado
+coo(:,1) = coo(:,1) - c;  % Transladar as coordenadas pra que a rota√ß√£o ocorra em torno do ponto desejado
 coo_R = (R*coo')';                  
-coo_R(:,1) = coo_R(:,1) + c;     % Mover a geometria de volta ‡ posiÁ„o original
+coo_R(:,1) = coo_R(:,1) + c;     % Mover a geometria de volta √† posi√ß√£o original
 
 end
